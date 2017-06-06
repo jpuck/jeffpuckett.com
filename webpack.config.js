3eeb7d2a60af1acc32385d1d58a4db6c7751f324
Jeff Puckett <jeff@jeffpuckett.com>
2017-06-06 03:11:06 +0000

    use html-webpack-plugin instead of strreplace

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-var VERSION_CSS = '0.1.0';
 var webpack = require('webpack');
 var inProduction = (process.env.NODE_ENV === 'production');
 var outdir = __dirname + '/docs';
@@ -7,17 +6,17 @@ const path = require('path');
 const glob = require('glob');
 const PurifyCSSPlugin = require('purifycss-webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
         jp: [
             './sass/main.scss',
-            './html/index.html',
         ]
     },
     output: {
         path: outdir,
-        filename: 'css/[name].[chunkhash:8].js'
+        filename: 'css/[name].[chunkhash].js'
     },
     module: {
         rules: [
@@ -27,39 +26,10 @@ module.exports = {
                     use: ['css-loader', 'sass-loader']
                 })
             },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {
-                            name: "[name].[ext]",
-                        },
-                    },
-                    {
-                        loader: 'string-replace-loader',
-                        query: {
-                            search: /\$VERSION_CSS/,
-                            replace: '/css/jp.'+VERSION_CSS+'.css'
-                        }
-                    },
-                    {
-                        loader: "extract-loader",
-                    },
-                    {
-                        loader: 'html-loader',
-                        options: {
-                            minimize: inProduction,
-                            removeComments: inProduction,
-                            collapseWhitespace: inProduction
-                        }
-                    }
-                ]
-            }
         ]
     },
     plugins: [
-        new ExtractTextPlugin('css/jp.'+VERSION_CSS+'.css'),
+        new ExtractTextPlugin('css/jp.[contenthash].css'),
         new PurifyCSSPlugin({
             // Give paths to parse for rules. These should be absolute!
             paths: glob.sync(path.join(__dirname, '/html/*.html')),
@@ -69,6 +39,14 @@ module.exports = {
             minimize: inProduction
         }),
         new CleanWebpackPlugin(outdir+'/css'),
+        new HtmlWebpackPlugin({
+            template: 'html/index.html',
+            filename: 'index.html',
+            minify: {
+                collapseWhitespace: inProduction,
+                removeComments: inProduction
+            }
+        })
     ]
 };
 
