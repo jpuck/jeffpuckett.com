b342bea240665c8f8ccc53e4aa56c45e24c39a25
Jeff Puckett <jeff@jeffpuckett.com>
2017-06-14 15:07:54 +0000

    add meta viewport

--- ruby/github-markup.rb
+++ ruby/github-markup.rb
@@ -4,6 +4,7 @@ file = 'markdown/resume.md'
 out = 'html/resume.html'
 
 File.open(out, 'w') do |output|
+    output.puts '<head><meta name="viewport" content="width=device-width, initial-scale=1"></head>'
     output.puts '<body class="markdown-body">'
     output.puts GitHub::Markup.render(file, File.read(file))
     output.puts '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script></body>'
