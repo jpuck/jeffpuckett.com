cff152c3e7a3b06b691035a88cd0b9283d509c0c
Jeff Puckett <jeff@jeffpuckett.com>
2017-06-15 03:06:39 +0000

    add updated timestamp

--- ruby/github-markup.rb
+++ ruby/github-markup.rb
@@ -1,4 +1,5 @@
 require 'github/markup'
+require 'time'
 
 file = 'markdown/resume.md'
 out = 'html/resume.html'
@@ -7,5 +8,6 @@ File.open(out, 'w') do |output|
     output.puts '<head><meta name="viewport" content="width=device-width, initial-scale=1"></head>'
     output.puts '<body class="markdown-body">'
     output.puts GitHub::Markup.render(file, File.read(file))
+    output.puts "<small style='float:right'>updated #{Time.now.utc.iso8601}</small>"
     output.puts '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script></body>'
 end
